3c180e5cf99d960d49152f68f44a98383d9426e4
Seiji Emery <seijiemery@gmail.com>
2016-02-22 14:18:04 -0800

    moved implementations to inner struct to get around aliasing between global/static and instance methods

--- src/text/font.d
+++ src/text/font.d
@@ -31,46 +31,48 @@ private class FontData {
 }
 
 private struct FontLoader {
-    private static __gshared FontLoader instance;
+    private static __gshared FontLoader.Instance instance;
     //private static __gshared auto mutex = new Mutex();
 
-    struct RawFontData {
-        ubyte[] contents;
-    }
-    alias FileData = ubyte[];
-
-    private FileData[string] loadedFiles;
-    private FontData[string] loadedFonts;
-
-    Signal!(string)          onFontFileLoaded;
-    Signal!(string,FontData) onFontLoaded;
-
-    FontData getFont (string fontPath, int fontIndex) {
-        auto index = format("%s:%d", fontPath, fontIndex);
-        if (index in loadedFonts) {
-            return loadedFonts[index];
+    static struct Instance {
+        struct RawFontData {
+            ubyte[] contents;
         }
-        if (fontPath !in loadedFiles) {
-            if (!exists(fontPath) || !attrIsFile(getAttributes(fontPath)))
-                throw new ResourceError("Invalid font file: '%s' does not exist", fontPath);
-            auto contents = cast(ubyte[])read(fontPath);
-            if (contents.length == 0)
-                throw new ResourceError("Invalid font file: '%s' file length is zero", fontPath);
-            loadedFiles[fontPath] = contents;
-            onFontFileLoaded.emit(fontPath);
+        alias FileData = ubyte[];
+
+        private FileData[string] loadedFiles;
+        private FontData[string] loadedFonts;
+
+        Signal!(string)          onFontFileLoaded;
+        Signal!(string,FontData) onFontLoaded;
+
+        FontData getFont (string fontPath, int fontIndex) {
+            auto index = format("%s:%d", fontPath, fontIndex);
+            if (index in loadedFonts) {
+                return loadedFonts[index];
+            }
+            if (fontPath !in loadedFiles) {
+                if (!exists(fontPath) || !attrIsFile(getAttributes(fontPath)))
+                    throw new ResourceError("Invalid font file: '%s' does not exist", fontPath);
+                auto contents = cast(ubyte[])read(fontPath);
+                if (contents.length == 0)
+                    throw new ResourceError("Invalid font file: '%s' file length is zero", fontPath);
+                loadedFiles[fontPath] = contents;
+                onFontFileLoaded.emit(fontPath);
+            }
+            auto font = new FontData();
+            font.contents = loadedFiles[fontPath];
+            font.fontPath = fontPath;
+            font.fontIndex = fontIndex;
+            auto offset = stbtt_GetFontOffsetForIndex(font.contents.ptr, fontIndex);
+            if (offset == -1)
+                throw new ResourceError("Invalid font file (could not get offset for index %d in '%s')", fontIndex, fontPath);
+            if (!stbtt_InitFont(&font.fontInfo, font.contents.ptr, offset))
+                throw new ResourceError("Invalid font file (stbtt failed to init font data: '%s', index %d)", fontPath, fontIndex);
+            loadedFonts[index] = font;
+            onFontLoaded.emit(fontPath, font);
+            return font;
         }
-        auto font = new FontData();
-        font.contents = loadedFiles[fontPath];
-        font.fontPath = fontPath;
-        font.fontIndex = fontIndex;
-        auto offset = stbtt_GetFontOffsetForIndex(font.contents.ptr, fontIndex);
-        if (offset == -1)
-            throw new ResourceError("Invalid font file (could not get offset for index %d in '%s')", fontIndex, fontPath);
-        if (!stbtt_InitFont(&font.fontInfo, font.contents.ptr, offset))
-            throw new ResourceError("Invalid font file (stbtt failed to init font data: '%s', index %d)", fontPath, fontIndex);
-        loadedFonts[index] = font;
-        onFontLoaded.emit(fontPath, font);
-        return font;
     }
     static auto getFont (string fontPath, int fontIndex) {
         synchronized /*(mutex)*/ { return instance.getFont(fontPath, fontIndex); }
@@ -78,46 +80,48 @@ private struct FontLoader {
 }
 
 struct FontRegistry {
-    private static __gshared FontRegistry instance;
+    private static __gshared FontRegistry.Instance instance;
     //private static __gshared auto mutex = new Mutex();
 
-    private struct FontId {
-        string path;
-        int index;
-    }
+    static struct Instance {
+        private struct FontId {
+            string path;
+            int index;
+        }
 
-    private FontId[string]   fontPaths;
-    private string[][string] fontFamilies;
+        private FontId[string]   fontPaths;
+        private string[][string] fontFamilies;
 
-    public void registerFont (string fontName, string fontPath, int fontIndex) {
-        if (fontName in fontPaths && fontPaths[fontName] != fontPath)
-            throw new ResourceError("FontRegistry: overriding font lookup for '%s': '%s' with '%s'", 
-                fontName, fontPaths[fontName], fontPath);
-        fontPaths[fontName] = FontId(fontPath, fontIndex);
-    }
-    public void registerFontFamily (string fontFamilyName, string[] listOfFontNames) {
-        fontFamilies[fontFamilyName] = listOfFontNames;
-    }
-    public auto getFontPath (string name) { 
-        if (name !in fontPaths)
-            throw new ResourceError("No registered font '%s'", name);
-        return fontPaths[name];
-    }
-    public auto getFontFamily (string name) {
-        if (name !in fontFamilies)
-            throw new ResourceError("No registered font family '%s'", name);
-        return fontFamilies[fontName];
+        public void registerFont (string fontName, string fontPath, int fontIndex) {
+            if (fontName in fontPaths && (fontPaths[fontName].path != fontPath || fontPaths[fontName].index != fontIndex))
+                throw new ResourceError("FontRegistry: overriding font lookup for '%s': '%s:%d' with '%s:%d'", 
+                    fontName, fontPaths[fontName].path, fontPaths[fontName].index, fontPath, fontIndex);
+            fontPaths[fontName] = FontId(fontPath, fontIndex);
+        }
+        public void registerFontFamily (string fontFamilyName, string[] listOfFontNames) {
+            fontFamilies[fontFamilyName] = listOfFontNames;
+        }
+        public auto getFontPath (string name) { 
+            if (name !in fontPaths)
+                throw new ResourceError("No registered font '%s'", name);
+            return fontPaths[name];
+        }
+        public auto getFontFamily (string name) {
+            if (name !in fontFamilies)
+                throw new ResourceError("No registered font family '%s'", name);
+            return fontFamilies[name];
+        }
     }
 
     // Global versions
-    static void registerFont (string fontName, string fontPath) {
-        synchronized /*(mutex)*/ { instance.registerFont(fontName, fontPath); }
+    static void registerFont (string fontName, string fontPath, int fontIndex) {
+        synchronized /*(mutex)*/ { instance.registerFont(fontName, fontPath, fontIndex); }
     }
     static void registerFontFamily (string fontFamilyName, string[] listOfFontNames) {
         synchronized /*(mutex)*/ { instance.registerFontFamily(fontFamilyName, listOfFontNames); }
     }
     static auto getFontPath (string fontName) {
-        synchronized /*(mutex)*/ { return instance.getFont(fontName); }
+        synchronized /*(mutex)*/ { return instance.getFontPath(fontName); }
     }
     static auto getFontFamily (string fontFamilyName) {
         synchronized /*(mutex)*/ { return instance.getFontFamily(fontFamilyName); }
