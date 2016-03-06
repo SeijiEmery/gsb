68fde47d0a015d3f2b6e61e5d241fe8d1e12a153
Seiji Emery <seijiemery@gmail.com>
2016-03-06 03:29:57 -0800

    Wrote some boilerplate to make wiring up input events nicer.

--- src/ui/testui.d
+++ src/ui/testui.d
@@ -21,39 +21,70 @@ class UITestModule {
     int lineSamples = 1;
 
     ISlot[] slots;
-    this () {
-        slots ~= g_mainWindow.onMouseMoved.connect((vec2 pos) {
-            lastPos = pos;
-        });
-        slots ~= g_mainWindow.onMouseButtonPressed.connect((Window.MouseButton evt) {
-            points ~= lastPos;
-        });
-        slots ~= g_mainWindow.onGamepadAxesUpdate.connect((float[] axes) {
+    struct KeyHandler { int key, allMods, anyMods; delegate(void) cb; }
+    KeyHandler[] keyHandlers;
 
-        });
-        slots ~= g_mainWindow.onScrollInput.connect((vec2 scroll) {
-            size += scroll.y;
-        });
+    private void setup () {
+        disconnectAllSlots();
+
+        int GLFW_MOD_ANY = GLFW_MOD_SHIFT | GLFW_MOD_CONTROL | GLFW_MOD_ALT | GLFW_MOD_SUPER;
+        void onMouseMoved (delegate(vec2) cb) {
+            slots ~= g_mainWindow.onMouseMoved.connect(cb);
+        }
+        void onMouseButtonPressed (int buttonMask, int allMods, int anyMods, delegate(void) cb) {
+            slots ~= g_mainWindow.onMouseButtonPressed.connect((Window.MouseButton evt) {
+                if (evt.button & buttonMask && (evt.mods == allMods || evt.mods & anyMods)) { cb(); }
+            });
+        }
+        void onScroll (delegate(vec2) cb) {
+            slots ~= g_mainWindow.onScrollInput.connect(cb);
+        }
+        keyHandlers.length = 0;
         slots ~= g_mainWindow.onKeyPressed.connect((Window.KeyPress evt) {
-            if (evt.key == '=' && (evt.mods & GLFW_MOD_SHIFT))
-                evt.key = '+';
-
-            if (evt.key == '+')
-                lineSamples += 1;
-            else if (evt.key == '-' && lineSamples > 0)
-                lineSamples -= 1;
-
-            if (evt.key == '+' || evt.key == '-')
-                log.write("Set lineSamples = %d", lineSamples);
+            foreach (handler; keyHandlers) {
+                if (evt.key == handler.key && (evt.mods == handler.allMods || evt.mods & anyMods))
+                    handler.cb();
+            }
         });
+        void onKeyPressed (int key, int allMods, int anyMods, delegate(void) cb) {
+            keyHandlers ~= KeyHandler(key, allMods, anyMods, cb);
+        }
+        void onGamepadAxes (delegate(float[]) cb) {
+            slots ~= g_mainWindow.onGamepadAxesUpdate.connect(cb);
+        }
+
+        onMouseMoved(pos => 
+            lastPos = pos
+        );
+        onMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT, 0, GLFW_MOD_ANY, => 
+            points ~= lastPos 
+        );
+        // '+' button (shift+'+'): increase line samples, '-' button: decrease samples
+        onKeyPressed('=', GLFW_MOD_SHIFT, 0, => {
+            lineSamples += 1; log.write("Set lineSamples = %d", lineSamples);
+        });
+        onKeyPressed('-', 0, GLFW_MOD_SHIFT, => {
+            lineSamples = max(lineSamples-1, 0); log.write("Set lineSamples = %d", lineSamples);
+        });
+        onScroll(scroll => size += scroll.y);
+    }
+
+    private void disconnectAllSlots () {
+        if (slots.length) {
+            foreach (slot; slots)
+                slot.disconnect();
+            slots.length = 0;
+        }
+    }
+    this () {
+        setup();
     }
     ~this () {
-        foreach (slot; slots)
-            slot.disconnect();
+        disconnectAllSlots();
     }
 
     void update () {
-        //DebugRenderer.drawTri(lastPos, Color("#fadd4c"), size);
+        DebugRenderer.drawTri(lastPos, Color("#fadd4c"), size, cast(float)lineSamples);
 
         if (points.length) {
             //foreach (point; points) {
