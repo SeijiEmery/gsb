b410bf243a0588873dfb646dc5e6fa8c9fa1920f
Seiji Emery <seijiemery@gmail.com>
2016-03-20 01:18:36 -0700

    refactored friend/foe to use an enum (fixes fire targeting), added ui elements

--- src/shadowgun/gametest.d
+++ src/shadowgun/gametest.d
@@ -13,10 +13,11 @@ import gsb.core.color;
 import gsb.core.window;
 import gl3n.linalg;
 
+import gsb.core.stats;
 import gsb.core.collision2d;
 
 import std.random;
-
+import std.algorithm;
 
 
 shared static this () {
@@ -53,6 +54,12 @@ float DAMAGE_FLASH_DURATION = 0.16;
 
 float AGENT_ALIGNMENT_DISTANCE = 40.0;
 
+auto TEXT_COLOR_WHITE = Color(1,1,1, 0.85);
+auto FONT = "menlo";
+auto SMALL_FONT_SIZE = 18.0;
+auto PLAYER_NAME_FONT_SIZE = 40.0;
+auto PLAYER_INFO_FONT_SIZE = 25.0;
+
 private mat3 gameToScreenSpaceTransform (float zoom = 1.0) {
     float s = CURRENT_SCALE_FACTOR = zoom / GAME_UNITS_PER_SCREEN * g_mainWindow.screenDimensions.x;
     return mat3.identity()
@@ -88,7 +95,9 @@ immutable Color[] AGENT_COLORS = [
 
 
 private class Agent : IGameRenderable {
-    Color color = ENEMY_COLOR;
+
+    AgentId owner = AgentId.ENEMY;
+    bool dead = false;
 
     vec2 position = vec2(0, 0);
     vec2 dir      = vec2(0, 0);
@@ -100,6 +109,13 @@ private class Agent : IGameRenderable {
     float timeSinceLastJumped = 0.0;
 
     float timeSinceTookDamage = 0.0;
+
+    @property bool isEnemy () {
+        return owner == AgentId.ENEMY;
+    }
+    @property bool isPlayer () {
+        return owner >= AgentId.PLAYER_1 && owner <= AgentId.PLAYER_4;
+    }
 
     void update (float speed) {
         //log.write("update: %s + %s * %0.2f (%s) = %s", position, dir, speed * DEFAULT_AGENT_MOVE_SPEED, dir * speed * DEFAULT_AGENT_MOVE_SPEED, 
@@ -113,15 +129,15 @@ private class Agent : IGameRenderable {
         //float colorInterp = timeSinceTookDamage > 0 ? 1 - t * t : 0;
         float colorInterp = timeSinceTookDamage > 0 ? (timeSinceTookDamage / DAMAGE_FLASH_DURATION) : 0.0;
 
-        auto c = Color(
-            color.r * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.g * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.b * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.a * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+        auto color = Color(
+            AGENT_COLORS[owner].r * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].g * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].b * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].a * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
         );
 
         auto tpos = transform * vec3(position, 1.0);
-        DebugRenderer.drawCircle(tpos.xy, AGENT_SIZE * CURRENT_SCALE_FACTOR, c, circleWidth, 
+        DebugRenderer.drawCircle(tpos.xy, AGENT_SIZE * CURRENT_SCALE_FACTOR, color, circleWidth, 
             cast(uint)numCirclePoints, 2.0);
 
         //log.write("draw: %s * transform = %s, size: %s * %0.2f = %s", position, tpos, AGENT_SIZE, CURRENT_SCALE_FACTOR, AGENT_SIZE * CURRENT_SCALE_FACTOR);
@@ -129,38 +145,50 @@ private class Agent : IGameRenderable {
     void takeDamage () {
         timeSinceTookDamage = DAMAGE_FLASH_DURATION;
     }
+    bool collidesWith (Agent other) {
+        return Collision2d.intersects(Collision2d.Circle(position, AGENT_SIZE), Collision2d.Circle(other.position, AGENT_SIZE));
+    }
+}
+
+private class DamageSystem : IGameSystem {
+    void run (GameState state, float dt) {
+        foreach (agent; state.agents) {
+            agent.timeSinceTookDamage -= dt;
+        }
+        foreach (agent; state.enemyAgents) {
+            foreach (player; state.playerAgents) {
+                if (agent.collidesWith(player)) {
+                    player.takeDamage();
+                }
+            }
+        }
+        foreach (fireLine; state.fireLines) {
+            if (fireLine.t < 0 && !fireLine.wasFired) {
+                auto line = Collision2d.LineSegment(fireLine.start, fireLine.dir * FIRE_LINE_LENGTH, FIRE_LINE_WIDTH);
+                foreach (agent; state.agents) {
+                    if (agent.owner != fireLine.owner && Collision2d.intersects(Collision2d.Circle(agent.position, AGENT_SIZE), line)) {
+                        agent.takeDamage();
+                    }
+                }
+                fireLine.wasFired = true;
+            }
+        }
+    }
 }
 
 private class FiringSystem : IGameSystem {
     void run (GameState state, float dt) {
         foreach (agent; state.agents) {
             if ((agent.timeSinceLastFired -= dt) < 0 && agent.wantsToFire) {
-                if (agent != state.player)
+                if (agent.isEnemy)
                     agent.timeSinceLastFired = uniform01!float() * 10.0;
                 else
                     agent.timeSinceLastFired = AGENT_FIRE_INTERVAL;
-                state.fireBurst(agent.position, agent.fireDir, agent.color);
+                state.fireBurst(agent.position, agent.fireDir, agent.owner);
             }
             if ((agent.timeSinceLastJumped -= dt) < 0 && agent.wantsToJump) {
                 agent.timeSinceLastJumped = AGENT_JUMP_INTERVAL;
                 agent.position += agent.dir * AGENT_JUMP_LENGTH;
-            }
-            //if (agent.timeSinceTookDamage > 0)
-            agent.timeSinceTookDamage -= dt;
-
-            if (agent != state.player && Collision2d.intersects(Collision2d.Circle(agent.position, AGENT_SIZE), Collision2d.Circle(state.player.position, AGENT_SIZE))) {
-                state.player.takeDamage();
-            }
-        }
-        foreach (line; state.fireLines) {
-            if (line.t < 0 && !line.wasFired) {
-                auto l = Collision2d.LineSegment(line.start, line.dir * FIRE_LINE_LENGTH, FIRE_LINE_WIDTH);
-                foreach (agent; state.agents) {
-                    if (agent != state.player && Collision2d.intersects(Collision2d.Circle(agent.position, AGENT_SIZE), l)) {
-                        agent.takeDamage();
-                    }
-                }
-                line.wasFired = true;
             }
         }
     }
@@ -173,7 +201,7 @@ private class EnemyPursuitSystem : IGameSystem {
 
         //log.write("%s", state.agents.map!("a.position.toString()").join(", "));
         foreach (agent; state.agents) {
-            if (agent == state.player)
+            if (agent.owner != AgentId.ENEMY)
                 continue;
 
             if (distance(agent.position, state.player.position) > 40.0 || (!agent.dir.x && !agent.dir.y)) {
@@ -182,7 +210,7 @@ private class EnemyPursuitSystem : IGameSystem {
 
             auto sep_force = vec2(0,0);
             foreach (neighbor; state.agents)
-                if (neighbor != state.player && neighbor.position != agent.position)
+                if (neighbor.owner == AgentId.ENEMY && neighbor.position != agent.position)
                     sep_force += (neighbor.position - agent.position);
 
             immutable float WEIGHT = 1.0;
@@ -212,39 +240,16 @@ private class EnemyPursuitSystem : IGameSystem {
     }
 }
 
-//private class FlockingEnemyController : IGameSystem {
-//    Agent target;
-//    Agent[] agents;
-
-//    this (Agent target) {
-//        this.target = target;
-//    }
-//    void add (Agent agent) {
-//        agents ~= agent;
-//    }
-//    void run (GameState state, float dt) {
-//        auto center = vec2(0, 0);
-//        foreach (agent; agents)
-//            center += agent.position;
-//        center /= cast(float)agents.length;
-
-//        foreach (i; 0 .. agents.length) {
-//            foreach (j; i+1 .. agents.length) {
-
-//            }
-//        }
-//    }
-//}
-
 
 private class FireLine {
+    AgentId owner;
     Color color;
     vec2 start, dir;
     float t, chargeDuration, staticDuration;
     bool wasFired = false;
 
-    this (Color color, vec2 start, vec2 dir, float chargeDuration, float staticDuration) {
-        this.color = color;
+    this (AgentId owner, vec2 start, vec2 dir, float chargeDuration, float staticDuration) {
+        this.owner = owner;
         this.start = start;
         this.dir = dir;
 
@@ -270,13 +275,13 @@ private class FireLine {
 
         colorInterp = colorInterp * colorInterp;
 
-        auto c = Color(
-            color.r * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.g * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.b * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
-            color.a * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+        auto color = Color(
+            AGENT_COLORS[owner].r * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].g * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].b * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
+            AGENT_COLORS[owner].a * 0.5 * (1 - colorInterp) + 1.0 * colorInterp,
         );
-        DebugRenderer.drawLines([ p1.xy, p2.xy ], c, FIRE_LINE_WIDTH * CURRENT_SCALE_FACTOR);
+        DebugRenderer.drawLines([ p1.xy, p2.xy ], color, FIRE_LINE_WIDTH * CURRENT_SCALE_FACTOR);
     }
 }
 
@@ -289,10 +294,18 @@ private class GameState {
 
     FireLine[] fireLines;
 
+    @property auto playerAgents () {
+        return agents.filter!"a.isPlayer"();
+    }
+    @property auto enemyAgents () {
+        return agents.filter!"a.isEnemy"();
+    }
+
     this (Agent player) {
         this.systems = [
             cast(IGameSystem)new FiringSystem(),
             cast(IGameSystem)new EnemyPursuitSystem(),
+            cast(IGameSystem)new DamageSystem(),
         ];
         this.agents = [ player ];
         this.player = player;
@@ -320,8 +333,8 @@ private class GameState {
             agent.draw(transform);
     }
 
-    void fireBurst (vec2 pos, vec2 dir, Color color) {
-        fireLines ~= new FireLine(color, pos, dir, FIRE_LINE_CHARGE_DURATION, FIRE_LINE_STATIC_DURATION);
+    void fireBurst (vec2 pos, vec2 dir, AgentId owner) {
+        fireLines ~= new FireLine(owner, pos, dir, FIRE_LINE_CHARGE_DURATION, FIRE_LINE_STATIC_DURATION);
     }
 
     void createEnemy (vec2 pos) {
@@ -335,8 +348,8 @@ private class PlayerController : IGameController {
     Agent player;
     GameState gameState;
 
-    this (Agent agent, GameState gameState) {
-        agent.color = PLAYER_COLOR;
+    this (Agent agent, AgentId owner, GameState gameState) {
+        agent.owner  = owner;
         this.player = agent;
         this.gameState = gameState;
     }
@@ -345,15 +358,7 @@ private class PlayerController : IGameController {
         event.handle!(
             (GamepadAxisEvent ev) {
                 player.dir = vec2(ev.AXIS_LX, ev.AXIS_LY);
-
-                //gameState.simSpeed = 1.0 - (ev.AXIS_RT - ev.AXIS_RY);
                 gameState.simSpeed = 1.0 - ev.AXIS_RT;
-                    //(abs(ev.AXIS_RT) > abs(ev.AXIS_RY) 
-                    //    ? ev.AXIS_RT : ev.AXIS_RY);
-
-                //gameState.simSpeed = 1.0 - ev.AXIS_RT;
-                log.write("%0.2f", gameState.simSpeed);
-                //log.write("%s", ev.axes[0..$].map!"a.to!string".join(", "));
 
                 if (player.wantsToFire && (ev.AXIS_LX || ev.AXIS_LY))
                     player.fireDir = vec2(ev.AXIS_LX, ev.AXIS_LY).normalized();
@@ -370,26 +375,102 @@ private class PlayerController : IGameController {
     }
 }
 
+private class GameUI {
+    UILayoutContainer[] containers;
+    UITextElement stats;
+    PlayerUI[4]   playerUI;
+
+    class PlayerUI {
+        AgentId owner = AgentId.PLAYER_1;
+        UILayoutContainer container;
+        UITextElement     score;
+        bool              isActive = false;
+
+        this (AgentId owner, string name, Layout layoutPos, bool isActive) {
+            this.owner = owner;
+            this.isActive = isActive;
+            container = new UILayoutContainer(LayoutDir.VERTICAL, layoutPos, vec2(5,5), 4, [
+                new UITextElement(vec2(),vec2(),vec2(5,5), name, new Font(FONT, PLAYER_NAME_FONT_SIZE), AGENT_COLORS[owner], Color()),
+                score = new UITextElement(vec2(),vec2(),vec2(5,5), "", new Font(FONT, PLAYER_INFO_FONT_SIZE), AGENT_COLORS[owner], Color()),
+            ]);
+        }
+        void update () {
+            if (isActive) {
+                container.dim = vec2(g_mainWindow.screenDimensions);
+                container.pos = vec2(0,0);
+                container.recalcDimensions();
+                container.doLayout();
+                container.render();
+            } else {
+                // hack: move offscreen to not render. Will add caching / state retention, etc later.
+                container.pos = vec2(g_mainWindow.screenDimensions) * 2;
+                container.doLayout();
+            }
+        }
+        void release () { if (container) { container.release(); container = null; } }
+    }
+
+    this () {
+        containers ~= new UILayoutContainer(LayoutDir.VERTICAL, Layout.TOP_CENTER, vec2(5,5), 3, [
+            stats = new UITextElement(vec2(),vec2(),vec2(1,1),"", new Font(FONT, SMALL_FONT_SIZE), TEXT_COLOR_WHITE, Color())
+        ]);
+        playerUI[0] = new PlayerUI(AgentId.PLAYER_1, "PLAYER 1", Layout.TOP_LEFT,  true);
+        playerUI[1] = new PlayerUI(AgentId.PLAYER_2, "PLAYER 2", Layout.TOP_RIGHT, false);
+        playerUI[2] = new PlayerUI(AgentId.PLAYER_3, "PLAYER 3", Layout.BTM_LEFT,  true);
+        playerUI[3] = new PlayerUI(AgentId.PLAYER_4, "PLAYER 4", Layout.BTM_RIGHT, true);
+    }
+    void release () {
+        if (containers.length) {
+            foreach (player; playerUI)
+                player.release();
+            foreach (container; containers)
+                container.release();
+            containers.length = 0;
+        }
+    }
+    void update () {
+        foreach (player; playerUI)
+            player.update();
+        foreach (container; containers) {
+            container.dim = vec2(g_mainWindow.screenDimensions);
+            container.recalcDimensions();
+            container.doLayout();
+            container.render();
+        }
+    }
+}
+
 private class GameModule : UIComponent {
     IGameController[] controllers;
     GameState gameState;
+    GameUI    ui;
 
     override void onComponentInit () {
         auto player = new Agent();
         gameState = new GameState(player);
+        ui = new GameUI();
 
-        controllers ~= new PlayerController(player, gameState);
+        controllers ~= new PlayerController(player, AgentId.PLAYER_1, gameState);
     }
     override void onComponentShutdown () {
         controllers.length = 0;
         gameState = null;
+        ui = null;
     }
     override void handleEvent (UIEvent event) {
         if (controllers.length) {
             event.handle!(
                 (FrameUpdateEvent ev) {
-                    gameState.update();
-                    gameState.draw();
+                    threadStats.timedCall("gamestate.update()", {
+                        gameState.update();
+                    });
+                    threadStats.timedCall("gamestate.draw()", {
+                        gameState.draw();
+                    });
+                    threadStats.timedCall("gamestate.ui()", {
+                        ui.stats.text = format("%d agents\nspeed %0.2f", gameState.agents.length, gameState.simSpeed);
+                        ui.update();
+                    });
                     return true;
                 },
                 (ScrollEvent ev) {
