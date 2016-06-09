2971713e037ab1bf95520331b49f28794ffe36ff
Seiji Emery <seijiemery@gmail.com>
2016-06-09 00:16:05 -0700

    cleanup

--- src/components/shadertoy.d
+++ src/components/shadertoy.d
@@ -1,179 +1,22 @@
 module gsb.components.shadertoy;
-import gsb.gl.debugrenderer;
+import gsb.core.slate2d;
 import gsb.core.uimanager;
 import gsb.core.uievents;
+import gsb.core.ui.uielements;
 import gsb.core.log;
-
-
-
+import gl3n.linalg;
 
 shared static this () {
-    UIComponentManager.runAtInit({
-        UIComponentManager.registerComponent(
-            new ShaderToyUI(), "shadertoy", true);
-    });
+    //UIComponentManager.runAtInit({
+    //    UIComponentManager.registerComponent(
+    //        new ShaderToyUI(), "shadertoy", true);
+    //});
 }
 
 private struct ShaderFileScanner {
-
+    void doScan () {}
 }
 
-interface Shape2d {
-    vec4 getRect ();
-    bool containsPoint (vec2 pt);
-    bool containsShape (Shape2d shape);
-
-    vec2 center ();
-    mat3 matrix_transform ();
-}
-
-interface ShapePhysics {
-    vec3 center_position ();
-    mat3 matrix_transform ();
-
-    void applyMotion (
-        double dt,
-        vec3 velocity,
-        vec3 angular_velocity );
-}
-
-interface GraphImpl {
-    void addChild (UIObj);
-    void setParent (UIObj);
-
-    UIObj getParent ();
-    void  visitChildren (void delegate(UIObj));
-    void  visitChildren (
-        bool delegate (UIObj) pred,
-        void delegate (UIObj) visitor
-    );
-}
-struct UIState {
-    bool mouseover = false;
-    bool clicked   = false;
-
-
-}
-
-
-
-
-class UIObj {
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-interface slateui_shape {
-    rect4f aabbRect ();
-    bool   containsPoint ()
-
-
-
-
-}
-
-
-
-class slateui_objmodel {
-    vec2 pos () {}
-    vec2 dim () {}
-
-
-
-}
-
-
-
-
-class slateui_baseobj {
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-class UIClickable (T) : T {
-    // private bool mouseover = false; -- inherited from UIElement
-    private bool pressed = false;
-
-    override bool handleEvents (UIEvent event) {
-        return event.handle!(
-            (MouseMoveEvent ev) {
-                auto prevMouseover = mouseover;
-
-            }
-
-
-
-        );
-    }
-
-
-
-}
-
-
-
-class UIButton (T) : T {
-    //private bool mouseover = false; -- inherited from UIElement
-    private bool pressed   = false;
-
-
-
-
-
-
-
-
-
-    private enum ButtonState { Mouseover }
-}
-
-
-
-
-
-
-struct UIButton {
-
-}
 private struct ShaderFileUI {
     UIElement root;
     UILayoutContainer fileList;
@@ -181,13 +24,6 @@ private struct ShaderFileUI {
 
     void createUI () {
         if (root) teardownUI();
-
-        root = new UIContainer(vec2(), vec2(), cast(UIElement[])[
-            new UITextElement()
-
-
-        ]);
-
     }
     void teardownUI () {
         if (root) {
---
+++ src/core/slate2d/package.d
@@ -0,0 +1,2 @@
+module gsb.core.slate2d;
+public import gsb.core.slate2d.renderer;
