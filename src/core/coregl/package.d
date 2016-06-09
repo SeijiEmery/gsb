bb836b3b225b4eba77e937b9ed7704ba0a2e4811
Seiji Emery <seijiemery@gmail.com>
2016-06-08 22:56:29 -0700

    removed unused gl code

--- src/core/coregl/package.d
+++ src/core/coregl/package.d
@@ -2,12 +2,12 @@
 module gsb.coregl;
 
 public import gsb.coregl.glerrors;
-public import gsb.coregl.batch;
+//public import gsb.coregl.batch;
 public import gsb.coregl.glstate;
 //public import gsb.coregl.glstats;
 public import gsb.coregl.interfaces;
 
-public import gsb.coregl.texture;
+//public import gsb.coregl.texture;
 //public import gsb.coregl.shader;
 public import gsb.coregl.vao;
 public import gsb.coregl.vbo;
--- src/core/coregl/sharedimpl.d
+++ src/core/coregl/sharedimpl.d
@@ -1,27 +1,26 @@
 
 module gsb.coregl.sharedimpl;
-import gsb.coregl.batch;
-import gsb.coregl.batchpool;
+//import gsb.coregl.batch;
+//import gsb.coregl.batchpool;
 public import gsb.coregl.glerrors;
 public import gsb.coregl.interfaces;
 
-bool isGraphicsThread () {
-    return false;
-}
+//bool isGraphicsThread () {
+//    return false;
+//}
 
-
-void gl_execImmediate (lazy void expr) {
-    if (isGraphicsThread())
-        expr();
-    else
-        pushImmediate(GLCommandBuffer.instance, { expr(); });
-}
-void gl_execNextFrame (lazy void expr) {
-    if (isGraphicsThread())
-        expr();
-    else
-        pushNextFrame(GLCommandBuffer.instance, { expr(); });
-}
+//void gl_execImmediate (lazy void expr) {
+//    if (isGraphicsThread())
+//        expr();
+//    else
+//        pushImmediate(GLCommandBuffer.instance, { expr(); });
+//}
+//void gl_execNextFrame (lazy void expr) {
+//    if (isGraphicsThread())
+//        expr();
+//    else
+//        pushNextFrame(GLCommandBuffer.instance, { expr(); });
+//}
 
 
 
