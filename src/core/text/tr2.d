4095647679b157af60a9b53b60c4235e7e2126ec
Seiji Emery <seijiemery@gmail.com>
2016-06-11 15:40:38 -0700

    finished richtext tests

--- src/core/text/tr2.d
+++ src/core/text/tr2.d
@@ -58,9 +58,11 @@ private struct RichTextParser {
     import std.regex;
 
     immutable string MATCH_NEWLINE = `\n+`;
-    immutable string MATCH_TEXT    = `(?:\\<|[^<\n])+`; // match any non-'<' or escaped '<'
+    immutable string MATCH_ESCAPE  = `\\[<>tn]`;
+    immutable string MATCH_TEXT    = `[^<\n\\]+`;
     immutable string MATCH_TAG     = `</?(\w+)(?:=([^>]+))?>`;
-    private auto r = ctRegex!(MATCH_TAG ~ `|` ~ MATCH_NEWLINE ~ `|` ~ MATCH_TEXT);
+    private auto r = ctRegex!(`^` ~ MATCH_TAG ~ `|` ~ MATCH_NEWLINE ~ `|`~ MATCH_ESCAPE ~ `|` ~ MATCH_TEXT);
+    private auto r_justText = ctRegex!(`^` ~ MATCH_ESCAPE ~ `|` ~ MATCH_TEXT );
 
     void setInput ( string text ) { m_input = text; }
     RTResult getNext () { 
@@ -70,28 +72,52 @@ private struct RichTextParser {
         auto c = matchFirst( m_input, r );
         m_input = c.post;
 
-        if (c.hit[0] == '\n') {
+        if (c.hit[0] == '<') {
+            if (c.hit[1] != '/') {
+                switch (c[1]) {
+                    case "b": return RTResult(RTCmd.SET_BOLD, "");
+                    case "i": return RTResult(RTCmd.SET_ITALIC, "");
+                    case "font": return RTResult(RTCmd.SET_FONT, c[2]);
+                    case "color": return RTResult(RTCmd.SET_COLOR, c[2]);
+                    case "size":  return RTResult(RTCmd.SET_SIZE, c[2]);
+                    default:
+                }
+            } else {
+                switch (c[1]) {
+                    case "b": return RTResult(RTCmd.END_BOLD, "");
+                    case "i": return RTResult(RTCmd.END_ITALIC, "");
+                    case "font": return RTResult(RTCmd.POP_FONT, "");
+                    case "color": return RTResult(RTCmd.POP_COLOR, "");
+                    case "size": return RTResult(RTCmd.POP_SIZE, "");
+                    default:
+                }
+            }
+        } else if (c.hit[0] == '\n') {
             return RTResult(RTCmd.NEWLINE, c.hit);
-        } else if (c.hit[0] == '<' && c.hit[1] != '/') {
-            switch (c[1]) {
-                case "b": return RTResult(RTCmd.SET_BOLD, "");
-                case "i": return RTResult(RTCmd.SET_ITALIC, "");
-                case "font": return RTResult(RTCmd.SET_FONT, c[2]);
-                case "color": return RTResult(RTCmd.SET_COLOR, c[2]);
-                case "size":  return RTResult(RTCmd.SET_SIZE, c[2]);
-                default:
-            }
-        } else if (c.hit[0] == '<') {
-            switch (c[1]) {
-                case "b": return RTResult(RTCmd.END_BOLD, "");
-                case "i": return RTResult(RTCmd.END_ITALIC, "");
-                case "font": return RTResult(RTCmd.POP_FONT, "");
-                case "color": return RTResult(RTCmd.POP_COLOR, "");
-                case "size": return RTResult(RTCmd.POP_SIZE, "");
-                default:
-            }
         }
-        return RTResult(RTCmd.TEXT, c.hit);
+        // Otherwise...
+
+        import gsb.core.log;
+
+        auto unescape (string s) {
+            if (s[0] == '\\' && (s[1] == '>' || s[1] == '<'))
+                return s[1..$];
+            return s;
+        }
+
+        // Accumulate remaining text + escapes
+        auto text = unescape(c.hit);
+        log.write("Text! %s", text);
+
+        while (m_input.length) {
+            auto c2 = matchFirst( m_input, r );
+            if (c2.hit[0] != '<' && c2.hit[0] != '\n') {
+                m_input = c2.post;
+                text ~= unescape(c2.hit);
+                log.write("Text! %s (%s)", text, c2.hit);
+            } else break;
+        }
+        return RTResult(RTCmd.TEXT, text);
     }
 
     string m_input;
@@ -106,6 +132,13 @@ unittest {
         }
     }
     RichTextParser p;
+
+    p.setInput("foo\\<bar\\>baz\n\n\n\\nborg\\>");
+    assertEq( p.getNext, RTResult(RTCmd.TEXT, "foo<bar>baz"));
+    assertEq( p.getNext, RTResult(RTCmd.NEWLINE, "\n\n\n"));
+    assertEq( p.getNext, RTResult(RTCmd.TEXT, "\\nborg>"));
+    assertEq( p.getNext, RTResult(RTCmd.END, ""));
+
     
     p.setInput("Foob\nBlarg<i>Foo</i>Bar<i><b>\nBaz\n</i>Borg</b>foo");
     assertEq( p.getNext, RTResult(RTCmd.TEXT, "Foob"));
@@ -139,9 +172,14 @@ unittest {
     assertEq( p.getNext, RTResult(RTCmd.TEXT, "bar"));
     assertEq( p.getNext, RTResult(RTCmd.POP_SIZE, ""));
     assertEq( p.getNext, RTResult(RTCmd.POP_FONT, ""));
-    assertEq( p.getNext, RTResult(RTCmd.TEXT, "Borg<foo>Blarg"));
+    assertEq( p.getNext, RTResult(RTCmd.TEXT, "Borg<foo>"));
+    assertEq( p.getNext, RTResult(RTCmd.SET_BOLD, ""));
+    assertEq( p.getNext, RTResult(RTCmd.TEXT, "Blarg"));
+    assertEq( p.getNext, RTResult(RTCmd.SET_ITALIC, ""));
     assertEq( p.getNext, RTResult(RTCmd.NEWLINE, "\n\n"));
     assertEq( p.getNext, RTResult(RTCmd.TEXT, "foob"));
+    assertEq( p.getNext, RTResult(RTCmd.END_BOLD, ""));
+    assertEq( p.getNext, RTResult(RTCmd.END_ITALIC, ""));
     assertEq( p.getNext, RTResult(RTCmd.END, ""));
 }
 
